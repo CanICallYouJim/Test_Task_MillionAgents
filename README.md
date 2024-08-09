960eeb1486635c1525ef8e94390d0fa102ac32f0
CanICallYouJim <129004917+CanICallYouJim@users.noreply.github.com>
2024-08-10 02:42:19 +0300

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Test_Task_MillionAgents
+Привет! Это тестовое задание. В нём реализованы загрузка и скачивание файлов, очистка их по крону. Файл сохраняется локально, копия отсылается в S3 хранилище, а метаданные идут в PostgreSQL.
+
+Инструкция:
+- git clone https://github.com/CanICallYouJim/Test_Task_MillionAgents.git
+  - в терминале войти в папку
+- pip install -r requirements.txt
+- создать базу в postgresql
+  - создать .env и .env.test с данными формата: DB_HOST = localhost
+  DB_PORT = 5432
+  DB_USER = postgres
+  DB_NAME = million_agents
+  DB_PASS = password
+  MODE = PROD ИЛИ TEST СООТВЕТСТВЕННО
+  
+- запустить через запуск main.py или команду в терминале fastapi run
--- files/cron_cleaning.py
+++ files/cron_cleaning.py
@@ -6,6 +6,7 @@ from sqlalchemy import delete, select
 
 from databases.database import async_session
 from databases.models import FileModel
+from files.s3 import s3
 
 
 async def clean_old_files():
@@ -14,22 +15,29 @@ async def clean_old_files():
     :param session: DB session
     """
     async with async_session() as session:
-        subquery = select(FileModel.id).order_by(FileModel.created_at.desc()).limit(3)
+        subquery = select(FileModel.id).order_by(FileModel.created_at.desc()).limit(15)
 
         filenames = await session.scalars(delete(FileModel).where(FileModel.id.not_in(subquery)).returning(FileModel.filename))
         await session.commit()
 
-    counter = 0
     filenames = filenames.all()
     script_dir = Path(__file__).parent
+
+    counter_local = 0
+    counter_s3 = 0
 
     for filename in filenames:
         try:
             file_path = script_dir / f"downloaded/{filename}"
+
             os.remove(file_path)
-            counter += 1
+            counter_local += 1
+
+            s3.delete_file(file_path)
+            counter_s3 += 1
+
         except Exception as ex:
             logging.error(ex)
             continue
 
-    logging.info(f"{len(filenames)} were deleted from DB, {counter} were deleted locally")
+    logging.info(f"{len(filenames)} were deleted from DB, {counter_local} were deleted locally, {counter_s3} from S3")
